8094a6b2280aea55120be08114d422f6cf23a5f0
AutisticFagetto420 <gm.terkeurs@student.alfa-college.nl>
2017-05-19 22:09:31 +0200

    Framework kinda sort of

---
+++ Framework_kinda/footer.php
@@ -0,0 +1,5 @@
+<footer>
+	Stijn ter Keurs
+</footer>
+</body>
+</html>
---
+++ Framework_kinda/index.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+<div id="fullpage">
+	<div class="section " id="section0">
+		
+	</div>
+	<div class="section" id="section1">
+		
+	</div>
+	<div class="section" id="section2">
+		
+	</div>
+	<div class="section" id="section3">
+
+	</div>
+</div>
+
+<script type="text/javascript" src="javascript.fullPage.js"></script>
+<script type="text/javascript">
+	fullpage.initialize('#fullpage', {
+		anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+		menu: '#menu',
+		css3:true
+	});
+
+</script>
+<?php get_footer(); ?>
